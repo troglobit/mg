troglobit/mg

commit 658bad71f9f52c2475931167f084cdc8621a830a

Add support for --disable-cscope to configure
This patch adds support for disabling Cscope support from mg. This
shaves ~10 kiB from the stripped binary.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

keymap.c
@@ -44,6 +44,7 @@ struct KEYMAPE (2) helpmap = {
 	}
 };
 
+#ifdef CSCOPE
 static PF cCsc[] = {
 	cscallerfuncs,		/* c */
 	csdefinition,		/* d */
@@ -93,6 +94,7 @@ struct KEYMAPE (2) ccmap = {
 		}
 	}
 };
+#endif /* CSCOPE */
 
 static PF cX4cF[] = {
 	poptofile,		/* ^f */
@@ -403,9 +405,11 @@ static struct KEYMAPE (8) fundmap = {
 	8,
 	selfinsert,
 	{
+#ifdef CSCOPE
 		{
 			CCHR('@'), CCHR('G'), fund_at, (KEYMAP *) & ccmap
 		},
+#endif
 		{
 			CCHR('H'), CCHR('H'), fund_h, (KEYMAP *) & helpmap
 		},

Makefile.am
@@ -8,7 +8,10 @@ mg_SOURCES      = autoexec.c basic.c bell.c buffer.c cinfo.c dir.c display.c		\
 # More or less standalone extensions.
 #
 # theo.c
-mg_SOURCES     += cmode.c cscope.c dired.c grep.c tags.c
+mg_SOURCES     += cmode.c dired.c grep.c tags.c
+if CSCOPE
+mg_SOURCES     += cscope.c
+endif
 mg_LDADD        = -ltermcap -lite
 dist_man1_MANS  = mg.1
 doc_DATA        = README

configure.ac
@@ -9,4 +9,11 @@ AC_PROG_CC
 AC_PROG_INSTALL
 AC_HEADER_STDC
 
+AC_ARG_ENABLE(cscope, AS_HELP_STRING([--disable-cscope], [Disable Cscope support]))
+
+AS_IF([test "x$enable_cscope" != "xno"], enable_cscope="yes"
+	AC_DEFINE(CSCOPE, 1, [Enable Cscope programming tool]))
+
+AM_CONDITIONAL(CSCOPE, [test "x$enable_cscope" = "xyes"])
+
 AC_OUTPUT

funmap.c
@@ -52,6 +52,7 @@ static struct funmap functnames[] = {
 	{cntmatchlines, "count-matches",},
 	{cntnonmatchlines, "count-non-matches",},
 #endif /* REGEX */
+#ifdef CSCOPE
 	{cscreatelist, "cscope-create-list-of-files-to-index",},
 	{csfuncalled, "cscope-find-called-functions",},
 	{csegrep, "cscope-find-egrep-pattern",},
@@ -65,6 +66,7 @@ static struct funmap functnames[] = {
 	{csnextmatch, "cscope-next-symbol",},
 	{csprevfile, "cscope-prev-file",},
 	{csprevmatch, "cscope-prev-symbol",},
+#endif /* CSCOPE */
 	{redefine_key, "define-key",},
 	{backdel, "delete-backward-char",},
 	{deblank, "delete-blank-lines",},

commit ffbd1a29a9b238716ebde02948c3d22aaaab4bda

Fix C-a, C-space, C-e regression from --disable-cscope patch
This fixes a regression introduced in 658bad7, the --disable-cscope
patch.  Where C-a, C-e, C-space (set mark), et al stopped working.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

keymap.c
@@ -421,6 +421,10 @@ static struct KEYMAPE (8) fundmap = {
 		{
 			CCHR('@'), CCHR('G'), fund_at, (KEYMAP *) & ccmap
 		},
+#else
+		{
+			CCHR('@'), CCHR('G'), fund_at, NULL
+		},
 #endif
 		{
 			CCHR('H'), CCHR('H'), fund_h, (KEYMAP *) & helpmap