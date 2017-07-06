7d3fcfddeef1ff07616ad68263fc59bf4fe5bf92
Ingo Schwarze <schwarze@usta.de>
2017-07-06 19:27:37 +0000

    Fix display of overlong lines containing non-ASCII bytes
    Also fixes a crash reported by Hiltjo Posthuma <hiltjo at codemadness
    dot org>, though in a different way than with the patch he sent.
    
    Signed-off-by: Ingo Schwarze <schwarze@usta.de>

--- display.c
+++ display.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: display.c,v 1.46 2015/03/24 22:28:10 bcallah Exp $	*/
+/*	$OpenBSD: display.c,v 1.48 2017/07/06 19:27:37 schwarze Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -363,10 +363,16 @@ vtpute(int c)
 	} else if (ISCTRL(c) != FALSE) {
 		vtpute('^');
 		vtpute(CCHR(c));
-	} else {
+	} else if (isprint(c)) {
 		if (vtcol >= 0)
 			vp->v_text[vtcol] = c;
 		++vtcol;
+	} else {
+		char bf[5], *cp;
+
+		snprintf(bf, sizeof(bf), "\\%o", c);
+		for (cp = bf; *cp != '\0'; cp++)
+			vtpute(*cp);
 	}
 }
 
