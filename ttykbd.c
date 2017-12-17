584b95460fbfbd949a98bea7b5be5e05622ad5be
Lari Rasku <lari.rasku@elisanet.fi>
2017-12-17 14:37:57 +0000

    Let the Insert key toggle overwrite mode
    There's a fairly strong convention among text editors that the Insert
    key should toggle overwrite mode.  This is admittedly far more common
    among GUI editors, but could mg adopt it as a default anyway?
    
    This matches GNU Emacs behavior.
    
    Signed-off-by: Lari Rasku <lari.rasku@elisanet.fi>

--- ttykbd.c
+++ ttykbd.c
@@ -51,6 +51,8 @@ ttykeymapinit(void)
 		dobindkey(fundamental_map, "scroll-up", key_npage);
 	if (key_ppage)
 		dobindkey(fundamental_map, "scroll-down", key_ppage);
+	if (key_ic)
+		dobindkey(fundamental_map, "overwrite-mode", key_ic);
 	if (key_dc)
 		dobindkey(fundamental_map, "delete-char", key_dc);
 
