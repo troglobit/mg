505eb8b52c382eaa612e4fe13d7926832d54c0c4
Reyk Flöter <reyk@openbsd.org>
2018-08-29 07:50:16 +0000

    Add set-case-replaced to toggle case-preserving replace on or off
    By default, replacing "foo" with "bar" turns "FOO" into "BAR".
    With case-replace turned off, "FOO" will turn into "bar".
    
    Signed-off-by: Reyk Flöter <reyk@openbsd.org>
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/def.h
+++ src/def.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: def.h,v 1.154 2016/01/02 10:39:19 lum Exp $	*/
+/*	$OpenBSD: def.h,v 1.156 2018/08/29 07:50:16 reyk Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -474,6 +474,7 @@ int		 ldelete(RSIZE, int);
 int		 ldelnewline(void);
 int		 lreplace(RSIZE, char *);
 char *		 linetostr(const struct line *);
+int		 setcasereplace(int, int);
 
 /* yank.c X */
 
--- src/funmap.c
+++ src/funmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: funmap.c,v 1.52 2015/12/29 19:44:32 lum Exp $	*/
+/*	$OpenBSD: funmap.c,v 1.54 2018/08/29 07:50:16 reyk Exp $	*/
 
 /* This file is in the public domain */
 
@@ -194,6 +194,7 @@ static struct funmap functnames[] = {
 #ifdef REGEX
 	{setcasefold, "set-case-fold-search",},
 #endif /* REGEX */
+	{setcasereplace, "set-case-replace",},
 	{set_default_mode, "set-default-mode",},
 	{setfillcol, "set-fill-column",},
 	{setmark, "set-mark-command",},
