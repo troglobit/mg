4a1ddb3aa158a9e2d8281427972debc6d326a2f8
Joachim Wiberg <troglobit@gmail.com>
2020-08-21 07:41:27 +0200

    Add Ctrl-arrow + Ctrl-PgUp/Dn to fundamental bindings
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/ttykbd.c
+++ src/ttykbd.c
@@ -23,6 +23,18 @@
 char	*keystrings[] = {NULL};
 
 /*
+ * Not available in termcap
+ */
+char	*key_cup    = "\e[1;5A";
+char	*key_cdown  = "\e[1;5B";
+char	*key_cright = "\e[1;5C";
+char	*key_cleft  = "\e[1;5D";
+char	*key_aright = "\e[1;3C";
+char	*key_aleft  = "\e[1;3D";
+char	*key_cpgup  = "\e[5;5~";
+char	*key_cpgdn  = "\e[6;5~";
+
+/*
  * Turn on function keys using keypad_xmit, then load a keys file, if
  * available.  The keys file is located in the same manner as the startup
  * file is, depending on what startupfile() does on your system.
@@ -56,6 +68,27 @@ ttykeymapinit(void)
 	if (key_dc)
 		dobindkey(fundamental_map, "delete-char", key_dc);
 
+	/* Local extensions, not available in termcap */
+	if (key_cup)
+		dobindkey(fundamental_map, "backward-paragraph", key_cup);
+	if (key_cdown)
+		dobindkey(fundamental_map, "forward-paragraph", key_cdown);
+	if (key_cright)
+		dobindkey(fundamental_map, "forward-word", key_cright);
+	if (key_cleft)
+		dobindkey(fundamental_map, "backward-word", key_cleft);
+
+	if (key_aright)
+		dobindkey(fundamental_map, "forward-word", key_aright);
+	if (key_aleft)
+		dobindkey(fundamental_map, "backward-word", key_aleft);
+
+	if (key_cpgup)
+		dobindkey(fundamental_map, "beginning-of-buffer", key_cpgup);
+	if (key_cpgdn)
+		dobindkey(fundamental_map, "end-of-buffer", key_cpgdn);
+
+	/* Check for $TERM specific .mg startup file */
 	if ((cp = getenv("TERM"))) {
 		if (((cp = startupfile(cp)) != NULL) && (load(cp) != TRUE))
 			ewprintf("Error reading key initialization file");
