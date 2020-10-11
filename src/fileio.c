29df310c6a64484188d9474d997b130d0ffbd27f
Joachim Wiberg <troglobit@gmail.com>
2020-10-11 09:41:30 +0200

    Not all UNIX have d_type in struct dirent, simplify
    On Solaris/Illumos/OmniOS we don't have the d_type member in struct dirent.
    Fortunately it turns out we can simplify the code to not look for d_type
    and increase the mg portability.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/fileio.c
+++ src/fileio.c
@@ -545,27 +545,22 @@ make_file_list(char *buf)
 	last = NULL;
 
 	while ((dent = readdir(dirp)) != NULL) {
-		int isdir;
+		char		statname[NFILEN + 2];
+		struct stat	statbuf;
+		int		isdir = 0;
+
 		if (strncmp(cp, dent->d_name, len) != 0)
 			continue;
-		isdir = 0;
-		if (dent->d_type == DT_DIR) {
-			isdir = 1;
-		} else if (dent->d_type == DT_LNK ||
-			    dent->d_type == DT_UNKNOWN) {
-			struct stat	statbuf;
-			char		statname[NFILEN + 2];
 
-			statbuf.st_mode = 0;
-			ret = snprintf(statname, sizeof(statname), "%s/%s",
-			    dir, dent->d_name);
-			if (ret < 0 || ret > (int)sizeof(statname) - 1)
-				continue;
-			if (stat(statname, &statbuf) < 0)
-				continue;
-			if (S_ISDIR(statbuf.st_mode))
-				isdir = 1;
-		}
+		statbuf.st_mode = 0;
+		ret = snprintf(statname, sizeof(statname), "%s/%s",
+		    dir, dent->d_name);
+		if (ret < 0 || ret > (int)sizeof(statname) - 1)
+			continue;
+		if (stat(statname, &statbuf) < 0)
+			continue;
+		if (S_ISDIR(statbuf.st_mode))
+			isdir = 1;
 
 		if ((current = malloc(sizeof(struct list))) == NULL) {
 			free_file_list(last);
