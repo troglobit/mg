troglobit/mg

commit 6def8a964476de8070f8709d49fef22d61092403

Clear status line after killing or switching buffers
This patch makes sure to clear the status/echo line after killing and
switching buffers by name.  Otherwise the kill/switch prompt lingers.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/buffer.c
@@ -252,6 +252,7 @@ killbuffer(struct buffer *bp)
 
 	free(bp->b_bname);			/* Release name block	 */
 	free(bp);				/* Release buffer block */
+	eerase();
 	return (TRUE);
 }
 
@@ -694,6 +695,7 @@ showbuffer(struct buffer *bp, struct mgwin *wp, int flags)
 				break;
 			}
 	wp->w_rflag |= WFMODE | flags;
+	eerase();
 	return (TRUE);
 }
 

commit 2c126f237f8481a33c956a98dcbfe73410cdfa2d

Fix regression; status line cleared when searching for file to open
While trying to open a file with C-x C-f and tabbing around in the file
system, the status line that displays the current path is cleared.  It
turns out this is a regression from 6def8a9.

This patch reverts part of 6def8a9 and implements a more correct fix to
the problem of lingering status message at switch/kill by name.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/buffer.c
@@ -73,6 +73,7 @@ int
 usebufname(const char *bufp)
 {
 	struct buffer *bp = NULL;
+	int rc;
 
 	if (bufp == NULL) {
 		if ((bp = bfind("*scratch*", TRUE)) == NULL)
@@ -84,7 +85,10 @@ usebufname(const char *bufp)
 
 	/* and put it in current window */
 	curbp = bp;
-	return (showbuffer(bp, curwp, WFFRAME | WFFULL));
+	rc = showbuffer(bp, curwp, WFFRAME | WFFULL);
+	eerase();
+
+	return rc;
 }
 
 /*
@@ -158,8 +162,9 @@ poptobuffer(int f, int n)
 int
 killbuffer_cmd(int f, int n)
 {
-	struct buffer *bp;
 	char    bufn[NBUFN], *bufp;
+	struct buffer *bp;
+	int rc;
 
 	if (f & FFRAND) /* dired mode 'q' */
 		bp = curbp;
@@ -170,7 +175,10 @@ killbuffer_cmd(int f, int n)
 		bp = curbp;
 	else if ((bp = bfind(bufn, FALSE)) == NULL)
 		return (FALSE);
-	return (killbuffer(bp));
+	rc = killbuffer(bp);
+	eerase();
+
+	return rc;
 }
 
 int
@@ -252,7 +260,7 @@ killbuffer(struct buffer *bp)
 
 	free(bp->b_bname);			/* Release name block	 */
 	free(bp);				/* Release buffer block */
-	eerase();
+
 	return (TRUE);
 }
 
@@ -695,7 +703,7 @@ showbuffer(struct buffer *bp, struct mgwin *wp, int flags)
 				break;
 			}
 	wp->w_rflag |= WFMODE | flags;
-	eerase();
+
 	return (TRUE);
 }
 