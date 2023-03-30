d821788f4335d71fd18e87b6bb7a9da5822ce705
Omar Polo <op@openbsd.org>
2023-03-30 08:07:07 +0000

    Trailing whitespace
    Signed-off-by: Omar Polo <op@openbsd.org>

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.90 2021/05/03 12:18:43 lum Exp $	*/
+/*	$OpenBSD: main.c,v 1.92 2023/03/30 08:07:07 op Exp $	*/
 
 /* This file is in the public domain. */
 
@@ -303,7 +303,7 @@ pty_init(void)
 	memset(&ws, 0, sizeof(ws));
 	ws.ws_col = 80,
 	ws.ws_row = 24;
-	
+
 	openpty(&master, &slave, NULL, NULL, &ws);
 	login_tty(slave);
 
