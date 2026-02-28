8d47f2a39583e3ce725fd8acc3811e692cde5c91
Omar Polo <op@openbsd.org>
2026-02-28 18:43:42 +0000

    Fix replace-regexp issues
     - an infinite loop with (replace-regexp "^.*$" "") because it does
       not advance to the next line
     - replace-regexp replaces all occurrences of ^pattern in a line
    
    Diff from Mark Willson, minor tweak by me: use gotoeol() instead of
    setting curwp->w_doto manually.  Upstream commit 07d1f22.
    
    Signed-off-by: Omar Polo <op@openbsd.org>
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/re_search.c
+++ src/re_search.c
@@ -193,6 +193,8 @@ retry:
 			ewprintf("<SP> replace, [.] rep-end, <DEL> don't, [!] repl rest <ESC> quit");
 			goto retry;
 		}
+		if (re_pat[0] == '^' && news[0] == '\0')
+			gotoeol(FFRAND, 1);
 	}
 
 stopsearch:
@@ -226,6 +228,8 @@ re_repl(int f, int n)
 		plen = regex_match[0].rm_eo - regex_match[0].rm_so;
 		if (re_doreplace((RSIZE)plen, news) == FALSE)
 			return (FALSE);
+		if (re_pat[0] == '^' && news[0] == '\0')
+			gotoeol(FFRAND, 1);
 		rcnt++;
 	}
 
@@ -344,10 +348,9 @@ re_forwsrch(void)
 	if (tbo == clp->l_used)
 		/*
 		 * Don't start matching past end of line -- must move to
-		 * beginning of next line, unless line is empty or at
-		 * end of file.
+		 * beginning of next line, unless at end of file.
 		 */
-		if (clp != curbp->b_headp && llength(clp) != 0) {
+		if (clp != curbp->b_headp) {
 			clp = lforw(clp);
 			tdotline++;
 			tbo = 0;
